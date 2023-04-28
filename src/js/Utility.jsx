12dc78fbd017e79d7c6026a23d15f940eb3805e4
Никита <immortallatrommi22@gmail.com>
2023-04-28 21:16:23 +0700

    Final commit 1

--- src/js/Utility.jsx
+++ src/js/Utility.jsx
@@ -1,22 +1,3 @@
-// Header-Scroll
-// let lastScroll = 0
-// const defaultOffset = 40
-// const defalutOffsetDelay = 2
-
-// const header = () => document.getElementById("header")
-// const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop
-// const containsHide = () => header().classList.contains("hide")
-
-// window.addEventListener("scroll", function() {
-//     if (scrollPosition() > lastScroll + defalutOffsetDelay && !containsHide() && scrollPosition() > defaultOffset) {
-//         header().classList.add("hide")
-//     } else if (scrollPosition() < lastScroll - defalutOffsetDelay && containsHide()) {
-//         header().classList.remove("hide")
-//     }
-
-//     lastScroll = scrollPosition()
-// })
-
 import { up, down } from "./components/redux/types"
 import store from "./components/redux"
 
